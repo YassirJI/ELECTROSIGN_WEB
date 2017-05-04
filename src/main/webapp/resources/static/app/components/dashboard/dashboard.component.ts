d441d67776316ca69b34b016f42d786d5733b861
Bouazza,Abdelkader,NNSA LAUSANNE,GLOBE-DSU-Sol Delivery-Dev Mgmt <Abdelkader.Bouazza@nespresso.com>
2017-05-04 11:16:53 +0100

    drag and drop of dashlet
    Dashlet displat fix

--- src/main/webapp/resources/static/app/components/dashboard/dashboard.component.ts
+++ src/main/webapp/resources/static/app/components/dashboard/dashboard.component.ts
@@ -9,6 +9,9 @@ import { DashletService } from './dashlet.service.js';
 
 @Component({
     selector: 'dashboard',
+    styles:[
+        '.dropIn { border: 1px dashed #AAA}',
+        ],
     templateUrl: 'app/components/dashboard/dashboard.component.html'
 })
 export class DashboardComponent implements OnInit {
@@ -22,7 +25,7 @@ export class DashboardComponent implements OnInit {
     public doughnutChartLabels:string[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
     public doughnutChartData:number[] = [350, 450, 100];
     public doughnutChartType:string = 'doughnut';
-     
+
     public barChartOptions:any = {
     scaleShowVerticalLines: false,
     responsive: true
@@ -30,12 +33,13 @@ export class DashboardComponent implements OnInit {
     public barChartLabels:string[] = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
     public barChartType:string = 'bar';
     public barChartLegend:boolean = true;
-    
+
     public barChartData:any[] = [
         {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
         {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'}
     ];
-    
+    private draggedItemid:Number=0;
+    private dropAreaItemid:Number=0;
     constructor(private _dashboardService: DashboardService, private _dashletService: DashletService) {
 
     }
@@ -44,19 +48,92 @@ export class DashboardComponent implements OnInit {
         this._dashboardService.getDashboards()
                 .subscribe(dashboards => this.dashboards = dashboards,
                            error => this.errorMessage = <any>error);
-               
         this.findDashlets(1);
-        
+
+    }
+
+    onDrag(draggEvent:DragEvent,dashlet:Dashlet):void{
+        draggEvent.dataTransfer.setData("text", dashlet.id+"");
+        this.draggedItemid=dashlet.id;
+    }
+
+    onDragEnter(draggEvent:DragEvent,dashlet:Dashlet):void{
+        this.dropAreaItemid=dashlet.id;
+        if(dashlet.id!==this.draggedItemid){//&& this.dropAreaItemid!==dashlet.id
+            let element = $("."+dashlet.id+"-dashlet");
+            element.addClass("dropIn");
+        }
+    }
+
+    onDragleave(draggEvent:Event,dashlet:Dashlet):void{
+        if(dashlet.id!==this.dropAreaItemid && this.dropAreaItemid!==this.draggedItemid){
+            let element = $("."+dashlet.id+"-dashlet");
+            element.removeClass("dropIn");
+        }
+    }
+
+    onDrop(draggEvent:DragEvent,dashlet:Dashlet):void{
+        let dropedDashletId:String = draggEvent.dataTransfer.getData("text");
+        let dropedInZoneId:String=dashlet.id+""; 
+        console.log(dropedDashletId); 
+        this.swapDashlet(dropedDashletId,dropedInZoneId);
+    }
+
+    onDragOver(draggEvent:DragEvent,dashlet:Dashlet):void{
+        if(dashlet.id!==this.draggedItemid){
+            draggEvent.preventDefault();
+        }
     }
 
     onChangeDashboard(dashboard:Dashboard) {
         this.selectedDashboard = dashboard;
         this.findDashlets(dashboard.id);
-  }
+    }
 
-  findDashlets(dashboardId:number): void {
-    this._dashletService.getDashlets(dashboardId)
-                    .subscribe(dashlets => this.selectedDashlets = dashlets,
-                            error => this.errorMessage = <any>error);
- }
+    findDashlets(dashboardId:number): void {
+        this._dashletService.getDashlets(dashboardId)
+                        .subscribe(dashlets => this.selectedDashlets = dashlets,
+                                error => this.errorMessage = <any>error);
+    }
+    
+    private swapDashlet(firstDashletId:String,secondDashletId:String): void {
+        let dropedElement=$("."+firstDashletId+"-dashlet");
+        let dropedInZoneElement=$("."+secondDashletId+"-dashlet");
+
+        let dropedElementClone=dropedElement.clone(true,true);
+        let dropedInZoneElementClone=dropedInZoneElement.clone(true,true);
+        
+        dropedElement.replaceWith(dropedInZoneElementClone);
+        dropedInZoneElement.replaceWith(dropedElementClone);
+
+        this.attacheDragDropEvent(dropedInZoneElementClone,this.getDashletById(secondDashletId));
+        this.attacheDragDropEvent(dropedElementClone,this.getDashletById(firstDashletId));
+        $(".dropIn").removeClass("dropIn");
+    }
+
+    private getDashletById(dashletId:String):Dashlet{
+        return this.selectedDashlets.find(dashlet => dashlet.id+""==dashletId);
+    }
+
+    private attacheDragDropEvent(DomObject:JQuery,dashlet:Dashlet){
+        var objectThis=this;
+        DomObject.unbind();
+        DomObject.bind({
+            dragstart: function(event:JQueryEventObject){
+                objectThis.onDrag(event.originalEvent as DragEvent,dashlet);
+            },
+            dragenter:function(event:JQueryEventObject){
+                objectThis.onDragEnter(event.originalEvent as DragEvent,dashlet);
+            },
+            dragleave:function(event:JQueryEventObject){
+                objectThis.onDragleave(event.originalEvent as DragEvent,dashlet);
+            },
+            dragover:function(event:JQueryEventObject){
+                objectThis.onDragOver(event.originalEvent as DragEvent,dashlet);
+            },
+            drop:function(event:JQueryEventObject){
+                objectThis.onDrop(event.originalEvent as DragEvent,dashlet);
+            }
+        });
+    }
 }
